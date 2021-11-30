e965d1909f0eb1ffd3e65cda2cf454f93fbb7469
BartDeraedt <93943562+BartDeraedt@users.noreply.github.com>
2021-11-30 12:43:29 +0100

    Add files via upload

---
+++ Test_rm/Test_rm/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_rm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var repository = new Repository();
+            bool validateRepositoryExists = repository.ValidateRepositoryExists();
+            if (!validateRepositoryExists)
+            {
+                repository.CreateRepository();
+            }
+
+        }
+
+        string sChannel;
+        private void btnNumber_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            sChannel += button.Content.ToString();
+            txtChannelSend.Text = sChannel;
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            var repository = new Repository();
+            repository.RegisterButtonClick(sChannel);
+            txtChannelSend.Text = "";
+            sChannel = "";
+        }
+
+        private void btnCommand_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            string sCommand=button.Content.ToString();
+            var repository = new Repository();
+            repository.RegisterButtonClick(sCommand);
+        }
+
+    }
+}
---
+++ test_tv/test_tv/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test_tv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker bgWorker; int progress;
+
+        TV tv = new TV();
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartWorker();
+        }
+        private void StartWorker()
+        {
+            bgWorker = new BackgroundWorker();
+            bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            bgWorker.ProgressChanged += new ProgressChangedEventHandler(bgWorker_ProgressChanged);
+            bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgWorker_RunWorkerCompleted);
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.WorkerSupportsCancellation = true;
+
+            progress = 1;
+            bgWorker.RunWorkerAsync();
+        }
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            while (true)
+            {
+
+                var repository = new Repository();
+                string sCommando = repository.ReadCommand();
+                if (sCommando != "No command")
+                {
+
+                    bgWorker.ReportProgress(progress, sCommando); // reports to "ProgressChanged"
+                } 
+
+                progress++;
+                if (progress == 100)
+                {
+                    progress = 1;
+                }
+
+                 repository.DeleteCommand();
+                 Thread.Sleep(100);
+            }
+        }
+        private void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // code here for updating progress
+            string sCommando = e.UserState.ToString();
+            Checkcommand(sCommando);
+
+        }
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // code here for shutting down tv (progress == 100)
+            MessageBox.Show("Shutting down...");
+            Close();
+        }
+
+
+
+        public void Checkcommand(string sCommando)
+        {
+            if (sCommando == "POWER")
+            {
+                Power();
+            }
+            else
+            {
+                if (tv.Power)
+                {
+
+                    switch (sCommando)
+                    {
+                        case "^":
+                            if (tv.Source == "TV")
+                            {
+                                tv.Channelup();
+                                txtblockChannelnr.Text = "Ch: " + tv.Channel.ToString();
+                            }
+                            break;
+                        case "v":
+                            if (tv.Source == "TV")
+                            {
+                                tv.Channeldown();
+                                txtblockChannelnr.Text = "Ch: " + tv.Channel.ToString();
+                            }
+                            break;
+                        case "+":
+                            tv.Volumeup();
+                            txtblockVolume.Text = "Vol: " + tv.Volume.ToString();
+                            break;
+                        case "-":
+                            tv.Volumedown();
+                            txtblockVolume.Text = "Vol: " + tv.Volume.ToString();
+                            break;
+                        case "SOURCE":
+                            tv.Sourcechange();
+                            if (tv.Source == "TV")
+                            {
+                                txtblockChannelnr.Text = "Ch: " + tv.Channel.ToString();
+                            }
+                            else
+                            {
+                                txtblockChannelnr.Text = tv.Source;
+                            }
+                            break;
+                        default:
+                            if (tv.Source == "TV")
+                            {
+                                tv.Channelnumber(int.Parse(sCommando));
+                                txtblockChannelnr.Text = "Ch: " + tv.Channel.ToString();
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void btnPower_Click(object sender, RoutedEventArgs e)
+        {
+            Power();
+        }
+
+        public void Power()
+        {
+            if (!tv.Power)
+            {
+                tv.Powerbutton();
+                btnPower.Background = Brushes.Green;
+                txtblockChannelnr.Text = "Ch: " + tv.Channel.ToString();
+                txtblockVolume.Text = "Vol: " + tv.Volume.ToString();
+            }
+            else
+            {
+                tv.Powerbutton(); 
+                btnPower.Background = Brushes.Gray;
+                txtblockChannelnr.Text = "";
+                txtblockVolume.Text = "";
+            }
+        }
+
+        private void TV_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            string sCommand = button.Content.ToString();
+            Checkcommand(sCommand);
+        }
+    }
+}
